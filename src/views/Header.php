322c5ba8236bd848b7b68accc1e120979ff24762
bkirilov <bkirilov@shutterfly.com>
2018-06-27 13:31:00 +0300

    Add responsive menu

--- src/views/Header.php
+++ src/views/Header.php
@@ -1,16 +1,21 @@
+<link rel="stylesheet" href="../assets/css/menu.css" />
+
 <header class="masthead mb-auto">
     <div class="inner">
       <h3 class="masthead-brand">Quotes</h3>
-      <nav class="nav nav-masthead justify-content-center">
-      <?php
-        if(isset($_SESSION['current_user_id'])){
-          echo '<a class="nav-link" href="../controllers/GetQuotes.php">Quotes</a>';
-          echo '<a class="nav-link" href="../controllers/Logout.php">Logout</a>';
-         } else{
-          echo '<a class="nav-link" href="./Register.php">Register</a>';
-          echo '<a class="nav-link" href="./Login.php">Login</a>';
-        }
-      ?>
+      <nav id="nav">
+        <ul>
+            <?php
+                if(isset($_SESSION['current_user_id'])){
+                echo '<li><a class="nav-link" href="../controllers/GetQuotes.php">Quotes</a></li>';
+                echo '<li><a class="nav-link" href="../controllers/Logout.php">Logout</a></li>';
+                } else{
+                echo '<li><a class="nav-link" href="./Register.php">Register</a></li>';
+                echo '<li><a class="nav-link" href="./Login.php">Login</a></li>';
+                }
+            ?>
+        </ul>
       </nav>
     </div>
 </header>
+<script src="../assets/js/menu.js"></script>
