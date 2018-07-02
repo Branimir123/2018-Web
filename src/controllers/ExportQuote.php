edf9dfd6f274d4d7dd995860a173e05b08d46885
Branimir123 <brani_svetoslavov@abv.bg>
2018-07-02 22:57:08 +0300

    Fix download

--- src/controllers/ExportQuote.php
+++ src/controllers/ExportQuote.php
@@ -1,13 +1,7 @@
 <?php
-    if(!isset($_SESSION['current_user_id'])) {
-        http_response_code(401);
-        $message = 'You have to be logged in to download quotes...';
-        header('Location: ../views/Login.php?message=' . $message . '&status_code=401');
-    }
-
     header('Content-Type: application/text');
     header('Content-Disposition: attachment; filename="'.$_GET['title'].'_'.$_GET['author'].'.txt"');
-    echo 'Title: ' . $_GET['quote_text'] . PHP_EOL .
-        'Quote: ' . $_GET['title'] . PHP_EOL . 
+    echo 'Title: ' . $_GET['title'] . PHP_EOL .
+        'Quote: ' . $_GET['quote_text'] . PHP_EOL . 
         'Author: ' . $_GET['author'] . PHP_EOL
 ?>
